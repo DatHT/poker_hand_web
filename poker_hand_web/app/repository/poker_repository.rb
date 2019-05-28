7aebafa768926c0c6515f38b8473a5671ff49141
フン・タン ダット <h.dat@mac0141.local>
2019-05-28 18:22:19 +0900

    update comment code

--- poker_hand_web/app/repository/poker_repository.rb
+++ poker_hand_web/app/repository/poker_repository.rb
@@ -5,12 +5,17 @@ module PokerRepository
   BASE_URL = 'https://datht-poker-api.herokuapp.com/api/v1/'
   POKER_CHECK = 'poker'
 
+  # APIを呼び出す
+  # @param input_data: "H1 H13 H12 H11 H10"
+  # @return hash { success: true, message: "message here" }
   def self.request_check_poker(input_data)
     response = RestClient.post BASE_URL + POKER_CHECK, {'cards' => [input_data]}.to_json, {content_type: :json, accept: :json}
-
     return analysis_response(JSON.parse(response.body))
   end
 
+  # APIの結果の分節
+  # @param response_body: APIの返却結果
+  # @return hash { success: true, message: "message here" }
   private
   def self.analysis_response(response_body)
     result_hash = { success: true }
